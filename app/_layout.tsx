44d3a1cf9cdd4e97775dfdf5de0689fbbaaf4b21
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2025-03-18 16:33:55 -0300

    teste

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,13 +14,13 @@ export default function Layout() {
   return (
     <View style={styles.container}>
       <Image
-        source={{ uri: 'https://cdn.discordapp.com/attachments/934175513637974086/1351584620688310272/Imagem_do_WhatsApp_de_2025-02-07_as_19.51.20_782f3ff3.jpg?ex=67dae8c4&is=67d99744&hm=bb54dafabaf146b94dd6e87380e9d36292d097d8944156e8f64936bd305dbc38&' }} // Substitua pela URL da sua imagem
+        source={{ uri: 'https://cdn.discordapp.com/attachments/934175513637974086/1351584620688310272/Imagem_do_WhatsApp_de_2025-02-07_as_19.51.20_782f3ff3.jpg?ex=67dae8c4&is=67d99744&hm=bb54dafabaf146b94dd6e87380e9d36292d097d8944156e8f64936bd305dbc38&' }} 
         style={styles.profileImage}
       />
       <Text style={styles.name}>Anderson Lucas</Text>
-      <Text style={styles.profession}>Desenvolvedor de Software</Text>
-      <Text style={styles.email}>seuemail@exemplo.com</Text>
-      <Text style={styles.phone}>‪+55 (11) 99999-9999‬</Text>
+      <Text style={styles.profession}>Analista de dados</Text>
+      <Text style={styles.email}>anderson.lucas.ribeiro101@gmail.com</Text>
+      <Text style={styles.phone}>‪+55 81991123596‬</Text>
 
       <View style={styles.socialIcons}>
         <Ionicons
@@ -50,20 +50,20 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   profileImage: {
-    width: 150,
-    height: 150,
+    width: 200,
+    height: 200,
     borderRadius: 75, 
     marginBottom: 20,
   },
   name: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#333', // Cor do texto escura
+    color: '#333', 
     marginBottom: 5,
   },
   profession: {
     fontSize: 16,
-    color: '#666', // Cor do texto cinza
+    color: '#666', 
     marginBottom: 10,
   },
   email: {
